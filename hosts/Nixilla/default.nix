d038535f8c3ab750b0105ea3825a527f36e8ba67
FabricSoul <fabric.soul7@gmail.com>
2025-01-04 14:34:07 -0500

    add second machine

--- hosts/Nixilla/default.nix
+++ hosts/Nixilla/default.nix
@@ -9,5 +9,18 @@
   ];
 
   networking.hostName = "Nixilla"; 
+  
+   hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  # Load VMware kernel modules
+  boot.kernelModules = [ "vmwgfx" ];
+
+  # Enable VMware guest services & video drivers
+  virtualisation.vmware.guest.enable = true;
+  services.xserver.videoDrivers = [ "vmware" ];
 
 }
